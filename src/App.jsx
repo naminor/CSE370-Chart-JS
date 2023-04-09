a70499282eff541962176cf93ffc282430c891b9
naminor <naminor@buffalo.edu>
2023-04-09 17:13:53 -0400

    Chart type can be changed from selection

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ const tenantURL = "https://webdev.cse.buffalo.edu/hci/api/api/example"
 const authToken = "example|lNvqxpTqdrUkE4UNrjVInxeAET4smq_a1JMS5K-ic_0"
 
 export default function App() {   
-    const [labels, setLabels] = useState(0)
-    const [datasets, setDatasets] = useState(0)
+    const [labels, setLabels] = useState([])
+    const [datasets, setDatasets] = useState([])
+    const [chartType, setChartType] = useState("doughnut")
 
     function getLabels() {
         setLabels(["comic", "sans"])
@@ -52,8 +53,15 @@ export default function App() {
 
     return (
         <div className="main-content">
-            {console.log(labels, datasets)}
-            <Chart type="doughnut" labels={labels} datasets={datasets}/>
+            <Chart type={chartType} labels={labels} datasets={datasets}/>
+            
+            <label for="chart-type">Chart Type:</label>
+            <select name="chart-type" onChange={(e) => setChartType(e.target.value)}>
+                <option value="bar">Bar</option>
+                <option value="line">Line</option>
+                <option value="pie">Pie</option>
+                <option value="doughnut">Doughnut</option>
+            </select> 
         </div>
     );
 }
