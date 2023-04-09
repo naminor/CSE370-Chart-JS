8a3d0bcc2039fb7fedd52f71e827fada790829f0
naminor <naminor@buffalo.edu>
2023-04-09 11:50:15 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+const tenantURL = "https://webdev.cse.buffalo.edu/hci/api/api/example"
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+                <div className="main-content">
+                </div>
+        );
+    }
+}
