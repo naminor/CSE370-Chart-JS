d9deb6b80ec7ce4de49357bbb77324453d840f73
naminor <naminor@buffalo.edu>
2023-04-09 13:23:27 -0400

    Starting implmentation for Chart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import { React, useState, useMemo } from "react";
+import Chart from "./components/Chart";
 import "./App.css";
 
 const tenantURL = "https://webdev.cse.buffalo.edu/hci/api/api/example"
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
+export default function App() {   
+    const [labels, setLabels] = useState(0)
+    const [datasets, setDatasets] = useState(0)
 
-        this.state = {
-        }
+    function getLabels() {
+        setLabels(["comic", "sans"])
     }
 
-    render() {
-        return (
-                <div className="main-content">
-                </div>
-        );
+    function getDatasets() {
+        setDatasets([{
+            label: "Popularity",
+            data: [100, 34]
+        }])
     }
+
+    useMemo(() => {
+        getLabels();
+        getDatasets();
+    }, []);
+
+    return (
+        <div className="main-content">
+            <Chart type="doughnut" labels={labels} datasets={datasets}/>
+        </div>
+    );
 }
---
+++ src/components/Chart.jsx
@@ -0,0 +1,30 @@
+import { React, useRef, useEffect } from "react";
+import { Chart as ChartJS } from "chart.js/auto";
+
+export default function Chart(props) {
+    const chart = useRef(0);
+    const canvas = useRef(0);
+    
+    useEffect(() => {
+        if (!canvas.current) return;
+        if (chart.current) chart.current.destroy();
+
+        chart.current = new ChartJS(canvas.current, {
+            type: props.type,
+            data: {
+                labels: props.labels,
+                datasets: props.datasets
+            },
+            options: {
+                responsive: 1
+            }
+        })
+    }, []);
+
+    return (
+        <div className="chart">
+            <canvas ref={canvas}/>
+        </div>
+    )
+
+}
